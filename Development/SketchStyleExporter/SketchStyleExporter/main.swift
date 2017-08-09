8704dfca2143f378b9d53c2d6cb44886c2a91f0a
Dylan Lewis <dylanslewis@me.com>
2017-08-09 22:38:45 +0100

    Create Xcode project

---
+++ Development/SketchStyleExporter/SketchStyleExporter/main.swift
@@ -0,0 +1,13 @@
+#!/usr/bin/swift
+//
+//  main.swift
+//  SketchStyleExporter
+//
+//  Created by Dylan Lewis on 09/08/2017.
+//  Copyright © 2017 Dylan Lewis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
