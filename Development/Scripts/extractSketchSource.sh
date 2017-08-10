74b3830635d505693616584a581379e907b29934
Dylan Lewis <dylanslewis@me.com>
2017-08-10 23:04:48 +0100

    Remove extraneous copy

--- Development/Scripts/extractSketchSource.sh
+++ Development/Scripts/extractSketchSource.sh
@@ -2,9 +2,7 @@
 
 name=$1
 
-cp "$name" SketchDocument.zip
 unzip "$name"
-rm SketchDocument.zip
 mv pages/* Development/SketchStyleExporter/SketchStyleExporter/SampleSketchSource/
 mv document.json Development/SketchStyleExporter/SketchStyleExporter/SampleSketchSource/
 rm meta.json
