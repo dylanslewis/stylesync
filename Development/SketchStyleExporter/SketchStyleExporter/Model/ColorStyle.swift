27ece2eb6e8832f60c20d6eda46668abcbeb9736
Dylan Lewis <dylanslewis@me.com>
2017-08-10 23:09:35 +0100

    Create ColorStyle model

---
+++ Development/SketchStyleExporter/SketchStyleExporter/Model/ColorStyle.swift
@@ -0,0 +1,14 @@
+//
+//  ColorStyle.swift
+//  SketchStyleExporter
+//
+//  Created by Dylan Lewis on 10/08/2017.
+//  Copyright © 2017 Dylan Lewis. All rights reserved.
+//
+
+import Cocoa
+
+struct ColorStyle {
+	let name: String
+	let color: NSColor
+}
