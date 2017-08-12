81922aae0931fa288a14afae94d6430966cb6d88
Dylan Lewis <dylanslewis@me.com>
2017-08-12 13:54:24 +0100

    Parse raw text style into a simplified model

--- Development/SketchStyleExporter/SketchStyleExporter/Model/TextStyle.swift
+++ Development/SketchStyleExporter/SketchStyleExporter/Model/TextStyle.swift
@@ -10,10 +10,29 @@ import Cocoa
 
 struct TextStyle {
 	let name: String
-	let kerning: Float
 	let fontName: String
-	let pointSize: Float
+	let pointSize: CGFloat
+	let kerning: CGFloat
 	let lineHeight: CGFloat
 	let color: NSColor
-	let colorStyle: ColorStyle?
+	
+	init?(textStyleObject: SketchDocument.TextStyles.Object) {
+		let textAttributes = textStyleObject.value.textStyle.encodedAttributes
+		
+		guard
+			let fontName = textAttributes.font.fontName,
+			let pointSize = textAttributes.font.pointSize,
+			let lineHeight = textAttributes.paragraphStyle.paragraphStyle?.maximumLineHeight,
+			let color = textAttributes.color.color
+		else {
+			return nil
+		}
+		
+		self.name = textStyleObject.name.camelcased
+		self.fontName = fontName
+		self.pointSize = pointSize
+		self.kerning = textAttributes.kerning
+		self.lineHeight = lineHeight
+		self.color = color
+	}
 }
