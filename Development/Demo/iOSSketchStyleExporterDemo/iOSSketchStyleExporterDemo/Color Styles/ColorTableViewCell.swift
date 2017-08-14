fad65049997207d62f82ee65d3ea3f57f3215f6c
Dylan Lewis <dylanslewis@me.com>
2017-08-14 19:44:36 +0100

    Move UIColor extensions to separate file

--- Development/Demo/iOSSketchStyleExporterDemo/iOSSketchStyleExporterDemo/Color Styles/ColorTableViewCell.swift
+++ Development/Demo/iOSSketchStyleExporterDemo/iOSSketchStyleExporterDemo/Color Styles/ColorTableViewCell.swift
@@ -72,15 +72,3 @@ extension ColorTableViewCell {
 		}
 	}
 }
-
-extension UIColor {
-	var components: (red: Int, green: Int, blue: Int, alpha: CGFloat)? {
-		var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
-		return getRed(&r, green: &g, blue: &b, alpha: &a) ? (Int((r*255).rounded()), Int((g*255).rounded()), Int((b*255).rounded()), a) : nil
-	}
-	
-	var hex: String? {
-		guard let components = components else { return nil }
-		return String(format: "%02X%02X%02X", components.red, components.green, components.blue)
-	}
-}
---
+++ Development/Demo/iOSSketchStyleExporterDemo/iOSSketchStyleExporterDemo/Extensions/UIColor+Extra.swift
@@ -0,0 +1,36 @@
+//
+//  UIColor+Extra.swift
+//  iOSSketchStyleExporterDemo
+//
+//  Created by Dylan Lewis on 14/08/2017.
+//  Copyright © 2017 Dylan Lewis. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+	var components: (red: Int, green: Int, blue: Int, alpha: CGFloat)? {
+		var red: CGFloat = 0
+		var green: CGFloat = 0
+		var blue: CGFloat = 0
+		var alpha: CGFloat = 0
+		guard getRed(&red, green: &green, blue: &blue, alpha: &alpha) == true else {
+			return nil
+		}
+		return (
+			roundedInteger(forComponent: red),
+			roundedInteger(forComponent: green),
+			roundedInteger(forComponent: blue),
+			alpha
+		)
+	}
+	
+	private func roundedInteger(forComponent component: CGFloat) -> Int {
+		return Int((component * 255).rounded())
+	}
+	
+	var hex: String? {
+		guard let components = components else { return nil }
+		return String(format: "%02X%02X%02X", components.red, components.green, components.blue)
+	}
+}
