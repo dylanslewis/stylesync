4faf7f4dcfd1882dd8fa407eed4fd6942e712bdd
Dylan Lewis <dylanslewis@me.com>
2017-09-10 21:37:20 +0100

    Update style guide to version 8.0

---
+++ Development/StyleSync/Sources/StyleSyncCore/Model/FileType.swift
@@ -0,0 +1,14 @@
+//
+//  FileName.swift
+//  StyleSyncPackageDescription
+//
+//  Created by Dylan Lewis on 02/09/2017.
+//
+
+import Foundation
+
+typealias FileType = String
+
+extension FileName.FileType {
+	static let json: FileName.FileType = "json"
+}
