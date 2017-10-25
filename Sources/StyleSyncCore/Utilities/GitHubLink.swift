3117f1b6c7fbf2c364322f2d355ee0c1e9f93760
Dylan Lewis <dylanslewis@me.com>
2017-10-25 22:06:22 +0100

    Add GitHubLink

---
+++ Sources/StyleSyncCore/Utilities/GitHubLink.swift
@@ -0,0 +1,16 @@
+//
+//  GitHubLink.swift
+//  StyleSyncCore
+//
+//  Created by Dylan Lewis on 25/10/2017.
+//
+
+import Foundation
+
+enum GitHubLink {
+	private static let baseURL = URL(string: "https://github.com/dylanlewis/StyleSync")!
+	
+	static let createIssue = baseURL.appendingPathComponent("issues/new")
+	static let templatesDirectory = baseURL.appendingPathComponent("issues/new") // FIXME: Use real URL
+	static let templateReadme = baseURL.appendingPathComponent("issues/new") // FIXME: Use real URL
+}
