fa106fdd4551cf4d894616e55f725a580b32d019
Dylan Lewis <dylanslewis@me.com>
2017-11-13 21:51:56 +0000

    Update Files to 2.0.0

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/JohnSundell/Files.git",
-			from: "1.0.0"
+			from: "2.0.0"
 		),
 		.package(
 			url: "https://github.com/JohnSundell/ShellOut.git",
